40f76a84b9ff2fa6bf98fe809b5d20e874494737
Ayan De <deayan252@gmail.com>
2025-05-05 00:26:10 +0530

    "search box added"

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 // import BorderLayout from "@/components/BorderLayout";
+import Searchbar from "@/components/Searchbar";
 import Sidenavbar from "@/components/Sidenavbar/Sidenavbar";
 
 export default function Home() {
   return (
-    <div className="md:flex">
+    <div>
       {/* <BorderLayout> */}
       <Sidenavbar />
       <h1 className="text-white text-2xl">Hello FixMyGali</h1>
+      <Searchbar />
       {/* </BorderLayout> */}
     </div>
   );
