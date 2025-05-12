3716778e90fefe30bfb0d215596d683312dc8b59
Ayan De <deayan252@gmail.com>
2025-05-12 23:49:35 +0530

    "popup dialog box completely set"

--- components/PinMarkerDialog.tsx
+++ components/PinMarkerDialog.tsx
@@ -6,10 +6,13 @@ import {
   DialogHeader,
   DialogTitle,
   DialogFooter,
+  DialogDescription,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useState } from "react";
+import Image from "next/image";
 
 type PinMarkerDialogProps = {
   open: boolean;
@@ -33,13 +36,30 @@ export default function PinMarkerDialog({
     <Dialog open={open} onOpenChange={onClose}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Fix it...</DialogTitle>
+          <DialogTitle>
+            <Image
+              src="/marker.png"
+              width={50}
+              height={50}
+              alt="Logo picure"
+              style={{ display: "block", margin: "0 auto" }}
+            />
+          </DialogTitle>
+          <DialogDescription>
+            Provide a label and upload a picture for the location your marking.
+          </DialogDescription>
         </DialogHeader>
+        <Label htmlFor="text">Text</Label>
         <Input
+          id="text"
           value={label}
           onChange={(e) => setLabel(e.target.value)}
           placeholder="E.g., Garbage dumped here"
         />
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="picture">Picture</Label>
+          <Input id="picture" type="file" />
+        </div>
         <DialogFooter>
           <Button onClick={handleSave}>Save</Button>
         </DialogFooter>
