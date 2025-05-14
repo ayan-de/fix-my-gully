da76e1a05d4043833189b17b9de92aaed1b9420c
Ayan De <deayan252@gmail.com>
2025-05-15 02:03:43 +0530

    "callbacks added"

--- auth.ts
+++ auth.ts
@@ -1,15 +1,38 @@
-import NextAuth from "next-auth"
-import authConfig from "@/auth.config"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { db } from "@/lib/db"
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
+import { getUserById } from "@/data/user";
 
 export const {
-    handlers: {GET,POST},
-    auth,
-    signIn,
-    signOut
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
 } = NextAuth({
-    adapter: PrismaAdapter(db),
-    session:{strategy:"jwt"},
-    ...authConfig,
-})
+  callbacks: {
+    // async signIn({ user }) {
+    //   const existingUser = await getUserById(user.id);
+
+    //   // if (!existingUser || !existingUser.emailVerified) {
+    //   //   return false;
+    //   // }
+    //   if (!existingUser) {
+    //     return false;
+    //   }
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
