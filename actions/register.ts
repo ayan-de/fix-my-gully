bfab57291d2c5ac651ea0f9d914c5089fb0b182a
Ayan De <deayan252@gmail.com>
2025-05-14 20:33:08 +0530

    "data getting inserted at db"

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,38 @@
 "use server";
+
+import bcrypt from "bcrypt";
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async(values: z.infer<typeof RegisterSchema>) => {
 const validatedFields = RegisterSchema.safeParse(values); 
-    if(!validatedFields){
+
+//if fields invalid
+    if(!validatedFields.success){
         return {error:"Invalid fields!"}
     }   
-return{success:"Email sent!"}
+
+    //if fields are not invalid we will extract the fields
+    const {email,password,name} = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password,10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if(existingUser) {
+        return {error:"Email already taken!"}
+    }
+
+    await db.user.create({
+        data:{
+            email,
+            password:hashedPassword,
+            name,
+            
+        },
+    });
+
+    //TODO: Send verification token email
+return{success:"User created!"}
 }
