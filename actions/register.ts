95189b7a4d605610e0bfca916d778fa5fe942caf
Ayan De <deayan252@gmail.com>
2025-05-17 21:16:56 +0530

    "verification token on registration"

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,7 @@ import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/token";
 
 export const register = async(values: z.infer<typeof RegisterSchema>) => {
 const validatedFields = RegisterSchema.safeParse(values); 
@@ -32,7 +33,8 @@ const validatedFields = RegisterSchema.safeParse(values);
             
         },
     });
+    const verificationToken = await generateVerificationToken(email);
 
     //TODO: Send verification token email
-return{success:"User created!"}
+return{success:"Verification email sent!"}
 }
